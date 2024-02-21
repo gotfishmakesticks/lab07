2c2fc7ce201a80365e4ca64e58f1281abaeae472
Алексей Миренков <aleksey.mirenkov1@gmail.com>
2024-02-21 22:35:01 +0300

    Finished program

--- app/src/main/java/g313/mirenkov/lab07/MainActivity.java
+++ app/src/main/java/g313/mirenkov/lab07/MainActivity.java
@@ -1,14 +1,97 @@
 package g313.mirenkov.lab07;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit_key, edit_value;
+    database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit_key = findViewById(R.id.edit_key);
+        edit_value = findViewById(R.id.edit_value);
+        db = new database(this, "qbase.db", null, 1);
+    }
+
+    public void on_select(View v){
+        String key = edit_key.getText().toString();
+        if (key.equals("")) {
+            Toast.makeText(this, "No key or value provided", Toast.LENGTH_LONG).show();
+            return;
+        }
+        String value = db.select(key);
+        if (value == null) {
+            Toast.makeText(this, "Key does not exist in this database", Toast.LENGTH_LONG).show();
+            return;
+        }
+        edit_value.setText(value);
+    }
+
+    public void on_insert(View v) {
+        String key = edit_key.getText().toString();
+        String value = edit_value.getText().toString();
+        if (key.equals("") || value.equals("")) {
+            Toast.makeText(this, "No key or value provided", Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            db.insert(key, value);
+            Toast.makeText(this, "Successfully inserted key", Toast.LENGTH_LONG).show();
+        } catch(Exception e) {
+            show_exception(e);
+        }
+    }
+
+    public void on_delete(View v) {
+        String key = edit_key.getText().toString();
+        if (key.equals("")) {
+            Toast.makeText(this, "No key provided", Toast.LENGTH_LONG).show();
+            return;
+        }
+        AlertDialog.Builder ad = new AlertDialog.Builder(this);
+        ad.setTitle("Deleting key");
+        ad.setMessage("Are you sure you want to delete this key?");
+        ad.setPositiveButton(android.R.string.yes, (dialog, which) -> {
+            try {
+                db.delete(key);
+                Toast.makeText(this, "Successfully deleted key", Toast.LENGTH_LONG).show();
+                edit_key.setText("");
+                edit_value.setText("");
+            }
+            catch(Exception e) {
+                show_exception(e);
+            }
+        });
+        ad.setNegativeButton(android.R.string.no, null);
+        ad.setIcon(android.R.drawable.ic_dialog_alert);
+        ad.show();
+    }
+
+    public void on_update(View v) {
+        String key = edit_key.getText().toString();
+        String value = edit_value.getText().toString();
+        if (key.equals("") || value.equals("")) {
+            Toast.makeText(this, "No key or value provided", Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            db.update(key, value);
+            Toast.makeText(this, "Successfully updated value", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            show_exception(e);
+        }
+    }
+
+    public void show_exception(Exception e) {
+        Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/g313/mirenkov/lab07/database.java
+++ app/src/main/java/g313/mirenkov/lab07/database.java
@@ -16,7 +16,7 @@ public class database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "create table tablica (qkey text primary key, qvalue text);";
+        String sql = "create table tablica (qkey text primary key, qvalue text)";
         sqLiteDatabase.execSQL(sql);
     }
 
@@ -25,22 +25,47 @@ public class database extends SQLiteOpenHelper {
 
     }
 
-    public void insert(String key, String value)
-    {
+    public void insert(String key, String value) throws Exception {
         SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-        String sql = String.format("insert into tablica values(%s, %s)", key, value);
+        if (select(key) != null)
+        {
+            throw new Exception("Key already exists in this database");
+        }
+        String sql = String.format("insert into tablica values('%s', '%s')", key, value);
         sqLiteDatabase.execSQL(sql);
     }
 
-    public String select(String key)
+    public @Nullable String select(String key)
     {
         SQLiteDatabase sqLiteDatabase = getReadableDatabase();
-        String sql = String.format("select qvalue from tablica where qkey = %s", key);
+        String sql = String.format("select qvalue from tablica where qkey = '%s'", key);
         Cursor cur = sqLiteDatabase.rawQuery(sql, null);
-
-        if (cur.moveToFirst())
+        if (!cur.moveToFirst())
         {
-
+            return null;
         }
+        return cur.getString(0);
+    }
+
+    public void delete(String key) throws Exception
+    {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        if (select(key) == null)
+        {
+            throw new Exception("Key does not exist in this database");
+        }
+        String sql = String.format("delete from tablica where qkey = '%s'", key);
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    public void update(String key, String value) throws Exception
+    {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        if (select(key) == null)
+        {
+            throw new Exception("Key does not exist in this database. If this is intended, instead use Insert");
+        }
+        String sql = String.format("update tablica set qvalue = '%s' where qkey = '%s'", value, key);
+        sqLiteDatabase.execSQL(sql);
     }
 }
